9fe2c5429eedc8a6c5f5bf0c67a9a4b90aee6b3f
sasth <sasthasankarcj@gmail.com>
2023-10-30 15:36:54 +0530

    a

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -21,6 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
       load = false;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     dynamic width = MediaQuery.sizeOf(context).width;
@@ -66,70 +67,113 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(
               height: 10,
             ),
-            load
-                ? Expanded(child: Center(child: CircularProgressIndicator()))
-                : Expanded(
-                    child: ListView.builder(
-                      itemCount: responseDataBbc?.articles?.length,
-                      scrollDirection: Axis.vertical,
-                      itemBuilder: (context, index) => Padding(
-                        padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
-                        child: InkWell(
-                          onTap: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => NewsScreen(
-                                    response: responseDataBbc
-                                        ?.articles?[index]),
-                              )),
-                          child: Container(
-                            height: 200,
-                            width: width,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    fit: BoxFit.fitWidth,
-                                    image: NetworkImage(responseDataBbc
-                                            ?.articles?[index].urlToImage
-                                            .toString() ??
-                                        "https://media4.giphy.com/media/3oEjI6SIIHBdRxXI40/200w.gif?cid=6c09b952wv0yphvhi6m54h8bejydufv91sz2de5quondqyvm&ep=v1_gifs_search&rid=200w.gif&ct=g")),
-                                borderRadius: BorderRadius.circular(20)),
-                            child: Column(
+            (responseDataBbc?.status == "error")
+                ? Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(50),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.grey),
+                        height: 120,
+                        width: 200,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
                               children: [
-                                Expanded(
-                                    child: Container(
-                                        decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(20)))),
-                                Expanded(
-                                    child: Container(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(12.0),
-                                    child: Column(
-                                      children: [
-                                        SizedBox(
-                                          height: 30,
-                                        ),
-                                        Text(
-                                          (responseDataBbc
-                                                  ?.articles?[index].title)
-                                              .toString(),
-                                          maxLines: 2,
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold),
-                                        )
-                                      ],
-                                    ),
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                                  child: Text(
+                                    'Oops!',
+                                    style: TextStyle(color: Colors.white, fontSize: 35,fontWeight: FontWeight.bold),
                                   ),
-                                )),
+                                ),
                               ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(0, 0, 15, 0),
+                                  child: ElevatedButton(onPressed: (){
+                                    loading();
+                                  }, child: Text("Retry"),style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Color(0xFF8775fc))),),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ))
+                : load
+                    ? Expanded(
+                        child: Center(child: CircularProgressIndicator()))
+                    : Expanded(
+                        child: ListView.builder(
+                          itemCount: responseDataBbc?.articles?.length,
+                          scrollDirection: Axis.vertical,
+                          itemBuilder: (context, index) => Padding(
+                            padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
+                            child: InkWell(
+                              onTap: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => NewsScreen(
+                                        response:
+                                            responseDataBbc?.articles?[index]),
+                                  )),
+                              child: Container(
+                                height: 200,
+                                width: width,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        fit: BoxFit.fitWidth,
+                                        image: NetworkImage(responseDataBbc
+                                                ?.articles?[index].urlToImage
+                                                .toString() ??
+                                            "https://media4.giphy.com/media/3oEjI6SIIHBdRxXI40/200w.gif?cid=6c09b952wv0yphvhi6m54h8bejydufv91sz2de5quondqyvm&ep=v1_gifs_search&rid=200w.gif&ct=g")),
+                                    borderRadius: BorderRadius.circular(20)),
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                        child: Container(
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        20)))),
+                                    Expanded(
+                                        child: Container(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(12.0),
+                                        child: Column(
+                                          children: [
+                                            SizedBox(
+                                              height: 30,
+                                            ),
+                                            Text(
+                                              (responseDataBbc
+                                                      ?.articles?[index].title)
+                                                  .toString(),
+                                              maxLines: 2,
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    )),
+                                  ],
+                                ),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
           ],
         ),
       ),
